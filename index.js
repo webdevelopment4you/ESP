c1d0b6fe00af5367e874a9d6d642f311e63d7038
webdevelopment4you <65403221+webdevelopment4you@users.noreply.github.com>
2025-01-25 19:07:49 +0530

    Updated with secondary mean DC Current

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', e => {
     let formElement = document.forms.myForm;
+    let formElementforcorona = document.forms.myFormforcorona;
     let resultDiv = document.getElementById('myResults');
     let suitableDiv = document.getElementById('displaySuccess');
     let notsuitableDiv = document.getElementById('displayDanger');
+    let suitableCorona = document.getElementById('displayCoronasuccess');
+    let notsuitableCorona = document.getElementById('displayCoronadanger');
     let formResults = document.getElementById('formResults');
     let fillDetails = document.getElementById('fillDetails');
+    let myFormforcorona = document.getElementById('myFormforcorona');
+    let avgDCBusvoltage = 60.5;
+    const submit1 = document.getElementById('submit1');
+    const submit2 = document.getElementById('submit2');
+    let displayCorona = document.getElementById('displayCorona')
 
-    formElement.addEventListener('submit', e => {
+    submit1.addEventListener('click', e => {
         e.preventDefault();
         formResults="";
         console.log(formResults);
@@ -19,6 +27,7 @@ document.addEventListener('DOMContentLoaded', e => {
         let lFields = formData.get('lFields');
         let hFields = formData.get('hFields');
         let Trsets = formData.get('Trsets');
+        let minCorona = formData.get('minCorona');
 
         let TCArea = 2* parseFloat(chambers) * parseFloat(lanes)* parseFloat(fields) * parseFloat(lFields) * parseFloat(hFields);
         console.log('TCArea', TCArea);
@@ -30,6 +39,9 @@ document.addEventListener('DOMContentLoaded', e => {
         console.log('number od TR Sets per Boiler', numberofTRSetsperboiler);
 
         let cAreaperTR = TCArea/numberofTRSetsperboiler;
+        console.log('collection area per TR Sets', cAreaperTR);
+
+        let meanDCCurrent = minCorona*cAreaperTR/avgDCBusvoltage;
         console.log('collection area per TR Sets', cAreaperTR);
 
         if (cAreaperTR<cArea) {
@@ -57,8 +69,34 @@ document.addEventListener('DOMContentLoaded', e => {
         document.getElementById('minTRSets').innerHTML = minTRSets;
         document.getElementById('numberofTRSetsperboiler').innerHTML = numberofTRSetsperboiler;
         document.getElementById('cAreaperTR').innerHTML = cAreaperTR;
+        document.getElementById('meanDCCurrent').innerHTML = meanDCCurrent;
         
         resultDiv.style.display = 'inherit';
+        myFormforcorona.style.display = 'inherit';
+        submit2.addEventListener('click', e => {
+            e.preventDefault();
+            let formData = new FormData(formElementforcorona);
+            let meanDCCurrentvalue = formData.get('meanDCCurrentvalue');
+            let coronaPower = meanDCCurrentvalue*avgDCBusvoltage/cAreaperTR;
+            console.log(coronaPower);
+            document.getElementById('coronaPower').innerHTML = coronaPower;
+            if (coronaPower<minCorona) {
+                if (suitableCorona.style.display='none') {
+                    notsuitableCorona.style.display = 'inherit';
+                } else {
+                    suitableCorona.style.display='none'
+                    notsuitableCorona.style.display = 'inherit';
+                }
+            } else {
+                if (notsuitableCorona.style.display = 'none') {
+                    suitableCorona.style.display = 'inherit';
+                } else {
+                    notsuitableCorona.style.display = 'none'
+                    suitableCorona.style.display = 'inherit';
+                }
+            }
+            displayCorona.style.display = 'inherit';
+        });
     });
 
 });
